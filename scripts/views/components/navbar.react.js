39ba033a0a64434f9b5a7fd2ca7f84a9b75bbb18
Alex Muresan <andrei.muresan@fortech.ro>
2017-10-31 14:52:56 +0200

    Recreated navbar html and js

--- scripts/views/components/navbar.react.js
+++ scripts/views/components/navbar.react.js
@@ -1,39 +1,119 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import NavbarAccordion from "./navbarAccordion.react";
+
+// import { Link } from "react-router-dom";
 
 class Navbar extends Component {
     constructor() {
         super();
         this.state = {
-            activeTab: "Countries",
-            navitems: [ "Countries", "Leagues", "Teams", "Players" ],
+            navitems: [
+                {
+                    label: "Home",
+                    slug: "/",
+                },
+                {
+                    label: "Players",
+                    children: [
+                        {
+                            label: "Players List",
+                            slug: "/players",
+                        },
+                        {
+                            label: "Create Player",
+                            slug: "/create-player",
+                        },
+                    ],
+                },
+                {
+                    label: "Teams",
+                    children: [
+                        {
+                            label: "Teams List",
+                            slug: "/teams",
+                        },
+                        {
+                            label: "Create Team",
+                            slug: "/create-team",
+                        },
+                    ],
+                },
+                {
+                    label: "Leagues",
+                    children: [
+                        {
+                            label: "Leagues List",
+                            slug: "/leagues",
+                        },
+                        {
+                            label: "Create League",
+                            slug: "/create-leagues",
+                        },
+                    ],
+                },
+                {
+                    label: "Countries",
+                    children: [
+                        {
+                            label: "Countries List",
+                            slug: "/countries",
+                        },
+                        {
+                            label: "Create Country",
+                            slug: "/create-country",
+                        },
+                    ],
+                },
+            ],
         };
-        this.setActiveTab = this.setActiveTab.bind( this );
-        this.createNavitems = this.createNavitems.bind( this );
+
+        this.buildNavItems = this.buildNavItems.bind( this );
+        this.buildItemsWithoutChildren = this.buildItemsWithoutChildren.bind( this );
     }
 
-    setActiveTab( tab ) {
-        this.setState( { activeTab: tab } );
-    }
-
-    createNavitems( item, index ) {
-        const { activeTab } = this.state;
-        const activeClass = item === activeTab ? "active" : "";
-        const link = item.toLowerCase();
+    buildNavItems() {
+        const { navitems } = this.state;
+        const items = navitems.map( ( item ) => {
+            const hasChildren = Array.isArray( item.children );
+            const navbaritem = hasChildren ?
+                <NavbarAccordion label={ item.label }>{ item.children }</NavbarAccordion>
+                :
+                this.buildItemsWithoutChildren( item );
+            return <div key={ item.label } >{ navbaritem }</div>;
+        } );
         return (
-            <li className={ activeClass } key={ index }><Link to={ `/${ link }` } onClick={ () => this.setActiveTab( item ) } >{ item }</Link></li>
+            <div className="navbar-items">
+                { items }
+            </div>
+        );
+    }
+
+    buildItemsWithoutChildren( item ) {
+        const { label, slug } = item;
+        const { history } = this.props;
+
+        return (
+            <div
+                className="navitem-clickable"
+                key={ slug }
+                onClick={ () => history.push( slug ) }
+                role="menuitem"
+                tabIndex="-1"
+            >
+                { label }
+            </div>
         );
     }
 
     render() {
-        const { navitems } = this.state;
+        const navbaritems = this.buildNavItems();
         return (
             <div className="navbar">
-                <ul className="clearfix">
-                    { navitems.map( this.createNavitems ) }
-                </ul>
+                { navbaritems }
             </div>
         );
     }
 }
-export default Navbar;
+
+export default withRouter( Navbar );
---
+++ scripts/views/components/navbarAccordion.react.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class NavbarAccordion extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isOpen: false,
+        };
+        this.handleClick = this.handleClick.bind( this );
+    }
+
+    handleClick( ) {
+        this.setState( {
+            isOpen: !this.state.isOpen,
+        } );
+    }
+
+    render() {
+        const { label, children, history } = this.props; // eslint-disable-line
+        const { isOpen } = this.state;
+        const openClassName = isOpen ? "open" : "";
+        const arrrow = isOpen ? "∧" : "∨";
+        return (
+            <div
+                className={ `navitem ${ openClassName }` }
+                key={ label }
+                onClick={ this.handleClick }
+                role="menuitem"
+                tabIndex="-1"
+            >
+                <div className="navitem-parent">
+                    <span>{ label }</span>
+                    <span>{ arrrow }</span>
+                </div>
+                <div className="navitem-children">
+                    {
+                        children.map( ( item, index ) => (
+                            <div
+                                className="navitem-child"
+                                key={ `${ item.slug } + ${ index }` } // eslint-disable-line
+                                onClick={ () => history.push( item.slug ) }
+                                role="menuitem"
+                                tabIndex="-1"
+                            >
+                                { item.label }
+                            </div>
+                        ) )
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withRouter( NavbarAccordion );
