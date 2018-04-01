957497471e02790d1ae5da102e92b2065cd34dec
Alex Muresan <Alex@192-168-0-107.rdsnet.ro>
2018-04-01 11:26:05 +0300

    Removed comment

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./scripts/application.react";
 import "./styles/style.scss";
 
-// test commit
 ReactDOM.render(
     <BrowserRouter>
         <App />
