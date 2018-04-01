c7c1f92df419416e9f8977706d2ba931570388af
Alex Muresan <Alex@192-168-0-107.rdsnet.ro>
2018-04-01 11:25:12 +0300

    Test commit

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./scripts/application.react";
 import "./styles/style.scss";
 
+// test commit
 ReactDOM.render(
     <BrowserRouter>
         <App />
