cc1dbde42370d7970c0e49ea15f977dcc3c0e170
Alex Muresan <andrei.muresan@fortech.ro>
2019-06-03 21:34:27 +0300

    Updated the way I display the players on playersDropdown

--- scripts/views/components/playersDropdown.react.js
+++ scripts/views/components/playersDropdown.react.js
@@ -15,14 +15,22 @@ class PlayersDropdown extends Component {
 
     buildOptions = player => (
         <option key={ player._id } value={ player._id }>
-            {player.fullName}
+            {`${ player.position } - ${ player.fullName }`}
         </option>
     );
 
     handleOptionPick = evt => this.setState( { selectedPlayer: evt.target.value } );
 
     render() {
-        const options = this.props.players.map( this.buildOptions );
+        const options = this.props.players.map( this.buildOptions ).sort( ( option1, option2 ) => {
+            if ( option1.props.children < option2.props.children ) {
+                return -1;
+            }
+            if ( option1.props.children > option2.props.children ) {
+                return 1;
+            }
+            return 0;
+        } );
         return (
             <div className="players-dropdown-container">
                 <span>{this.props.label}</span>
